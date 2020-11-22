85d1f8ec976a981f972c1490a27d3dff6fa9a530
LalRaghavenderGoud <61916480+LalRaghavenderGoud@users.noreply.github.com>
2020-11-22 20:41:15 +0530

    Quality DashBoard Java Service

---
+++ Java/QualityDashBoard/src/main/java/com/dashboard/quality/QualityDashBoardApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard.quality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityDashBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityDashBoardApplication.class, args);
+	}
+
+}
---
+++ Java/QualityDashBoard/src/test/java/com/dashboard/quality/QualityDashBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityDashBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
